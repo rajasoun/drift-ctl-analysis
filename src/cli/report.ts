22d36b2f01d1f2d3348985998ffcc3a469b7fd50
Raja Soundaramourty <rajasoun@icloud.com>
2022-12-08 12:18:15 +0530

    initial checkin

--- src/cli/report.ts
+++ src/cli/report.ts
@@ -3,20 +3,24 @@ import { command, run, string, option } from 'cmd-ts';
 import { buildReport } from '../lib/report'
 
 // Refernce: https://cmd-ts.vercel.app/getting_started.html
-const flags = option({
+const reportCommandFlags = option(
+  {
     type: string,
     long: 'env',
     short: 'e',
     description: "Provide Env :  (dev | qa | stage | prod)\n \t\tExample: npm run report -e dev"
-  });
-
-export const app = command({
-  name: 'report',
-  args: { env: flags,},
-  handler: ({ env,}) => {
-        const report = buildReport(env)
-        console.log(report)
   },
-});
+);
 
-run(app, process.argv.slice(2));
+export const reportApp = command(
+  {
+    name: 'report',
+    args: { env: reportCommandFlags,},
+    handler: ({ env,}) => {
+          const report = buildReport(env)
+          console.log(report)
+    },
+  },
+);
+
+run(reportApp, process.argv.slice(2));
