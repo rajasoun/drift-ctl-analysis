796490bf62444a4d551c694016d770f347da0371
Raja Soundaramourty <rajasoun@icloud.com>
2022-12-08 21:58:39 +0530

    initial checkin

--- src/cli/run.ts
+++ src/cli/run.ts
@@ -3,10 +3,11 @@ import { NewReportApp, NewReportCommand } from './report';
 
 // function to run command
 
-export function Run() {
+export function RunReportCommand() {
   const reportApp = NewReportApp();
   const reportCmd: CommandConfig = NewReportCommand();
   AddCommand(reportApp, reportCmd);
   reportApp.parse();
 }
-Run();
+
+RunReportCommand();
