f9c8e40d712663886a185fa709becb9fbf7d6b3a
Raja Soundaramourty <rajasoun@icloud.com>
2022-12-08 11:52:51 +0530

    initial checkin

--- src/cli/report.ts
+++ src/cli/report.ts
@@ -3,23 +3,17 @@ import { command, run, string, option } from 'cmd-ts';
 import { buildReport } from '../lib/report'
 
 // Refernce: https://cmd-ts.vercel.app/getting_started.html
-
-const help = "Provide Env :  (dev | qa | stage | prod)\n " 
-            + "\t\tExample: npm run report -e dev";
 const flags = option({
     type: string,
     long: 'env',
     short: 'e',
-    description: help
+    description: "Provide Env :  (dev | qa | stage | prod)\n \t\tExample: npm run report -e dev"
   });
-
 
 const app = command({
   name: 'drift-ctl-report',
-  args: {
-    env: flags,
-  },
-    handler: ({ env,}) => {
+  args: { env: flags,},
+  handler: ({ env,}) => {
         const report = buildReport(env)
         console.log(report)
   },
